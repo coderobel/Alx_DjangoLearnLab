769a2c96bae27f353a9862e7c298f5cc5b4eb7f3
Robel Fekadu <robel.ugr-4552-16@aau.edu.et>
2025-02-22 11:32:05 +0300

    django

--- django-models/LibraryProject/relationship_app/query_samples.py
+++ django-models/LibraryProject/relationship_app/query_samples.py
@@ -1,7 +1,16 @@
-from .models import Book, Author
+from .models import Book, Author, Library
+
+# Define or pass library_name
+library_name = 'Central Library'  # Example library name
 
 # First, get the Author object for 'John Doe'
 author = Author.objects.get(name='John Doe')
 
 # Then, filter the books by this Author object
 books_by_author = Book.objects.filter(author=author)
+
+# Get the Library object
+library = Library.objects.get(name=library_name)
+
+# Retrieve all books in the specific library
+all_books_in_library = library.books.all()
