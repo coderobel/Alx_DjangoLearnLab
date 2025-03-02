368a2049015f0eda592424594405712b6c6e35c1
Robel Fekadu <robel.ugr-4552-16@aau.edu.et>
2025-03-02 10:48:51 +0300

    django

--- advanced_features_and_security/LibraryProject/bookshelf/models.py
+++ advanced_features_and_security/LibraryProject/bookshelf/models.py
@@ -1,11 +1,32 @@
 from django.db import models
 from django.contrib.auth.models import AbstractUser
+from django.contrib.auth.models import BaseUserManager
 # Create your models here.
 
 class Book(models.Model):
     title = models.CharField(max_length=200)
     author = models.CharField(max_length=100)
-    publication_year = models.IntegerField
+    publication_year = models.IntegerField()
 class CustomUser(AbstractUser):
     date_of_birth = models.CharField(max_length=20)
     profile_photo = models.CharField(max_length = 20)
+class CustomUserManager(BaseUserManager):
+    def create_user(self,date_of_birth,profile_photo)
+        if not email:
+            raise ValueError('The Email field must be set')
+        email = self.normalize_email(email)
+        user = self.model(email=email, date_of_birth=date_of_birth, profile_photo=profile_photo, **extra_fields)
+        user.set_password(password)
+        user.save(using=self._db)
+        return user
+
+    def create_superuser(self, email, date_of_birth, profile_photo, password=None, **extra_fields):
+        extra_fields.setdefault('is_staff', True)
+        extra_fields.setdefault('is_superuser', True)
+
+        if extra_fields.get('is_staff') is not True:
+            raise ValueError('Superuser must have is_staff=True.')
+        if extra_fields.get('is_superuser') is not True:
+            raise ValueError('Superuser must have is_superuser=True.')
+
+        return self.create_user(email, date_of_birth, profile_photo, password, **extra_fields)
