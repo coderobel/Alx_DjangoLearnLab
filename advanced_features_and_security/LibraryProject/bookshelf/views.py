bf0082f34baa1ad061390ef97c31c97059ac5dc4
Robel Fekadu <robel.ugr-4552-16@aau.edu.et>
2025-03-02 11:42:57 +0300

    django

--- advanced_features_and_security/LibraryProject/bookshelf/views.py
+++ advanced_features_and_security/LibraryProject/bookshelf/views.py
@@ -1,3 +1,13 @@
-from django.shortcuts import render
+from django.shortcuts import render, get_object_or_404
+from django.http import HttpResponse
+from .models import Book
 
 # Create your views here.
+def book_list(request):
+    books = Book.objects.all()
+    return render(request, 'bookshelf/book_list.html', {'books': books})
+def raise_exception(request):
+    try:
+        pass
+    except Exception as e:
+        return HttpResponse(f"An error occurred: {e}")
