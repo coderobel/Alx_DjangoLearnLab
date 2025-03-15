23194599e719fd96924914667557039f42f24c6d
Robel Fekadu <robel.ugr-4552-16@aau.edu.et>
2025-03-15 13:15:26 +0300

    django

--- advanced-api-project/api/serializers.py
+++ advanced-api-project/api/serializers.py
@@ -15,7 +15,7 @@ class BookSerializer(serializers.ModelSerializer):
         return value
 
 class Authorserializer(serializers.ModelSerializer):
-    books = BookSerializer(many = True, read_only = True)
+    books = BookSerializer(many=True, read_only=True)
 
     class Meta:
         model = Author
