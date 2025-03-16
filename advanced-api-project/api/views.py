c674f859a7b473f7955e9b41094c4a2d1c17b41d
Robel Fekadu <robel.ugr-4552-16@aau.edu.et>
2025-03-16 13:26:27 +0300

    django

--- advanced-api-project/api/views.py
+++ advanced-api-project/api/views.py
@@ -1,5 +1,7 @@
 from django.shortcuts import render
-from rest_framework import permissions, generics
+from django_filters import rest_framework
+from rest_framework import permissions
+from rest_framework import generics
 from rest_framework.permissions import IsAuthenticatedOrReadOnly, IsAuthenticated
 from .models import Book
 from .serializers import BookSerializer,BookFilter
