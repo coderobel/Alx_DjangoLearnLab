a434dff392843ccf24f4e6d20f726bbf2218c4b5
Robel Fekadu <robel.ugr-4552-16@aau.edu.et>
2025-03-19 17:30:45 +0300

    django

--- django_blog/blog/urls.py
+++ django_blog/blog/urls.py
@@ -1,9 +1,15 @@
 from django.urls import path, include
 from django.views.generic import TemplateView
 from .views import SignUpView
+from django.contrib.auth import views as auth_views
+from . import views
+
 
 urlpatterns = [
-    path('accounts/', include('django.contrib.auth.urls')),
-    path('accounts/profile', TemplateView.as_view(template_name = 'accounts/profile.html'), name = 'profile'),
-    path('signup/', SignUpView.as_view(),name = 'templates/registration/signup'),
+   # path('accounts/', include('django.contrib.auth.urls')),
+    #path('accounts/profile', TemplateView.as_view(template_name = 'accounts/profile.html'), name = 'profile'),
+    #path('signup/', SignUpView.as_view(),name = 'templates/registration/signup'),
+    path('register/', views.register, name='register'),
+    path('login/', auth_views.LoginView.as_view(template_name='login.html'), name='login'),
+    path('logout/', auth_views.LogoutView.as_view(), name='logout'),
 ]
--- django_blog/blog/views.py
+++ django_blog/blog/views.py
@@ -7,3 +7,10 @@ class SignUpView(CreateView):
     form_class = UserCreationForm
     success_url = reverse_lazy('login')
     template_name = 'registration/signup.html'
+
+class CustomUserCreationForm(UserCreationForm):
+    email = forms.EmailField(required=True)
+
+    class Meta:
+        model = User
+        fields = ('username', 'email', 'password1', 'password2')
