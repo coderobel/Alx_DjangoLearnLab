698fb46f2ac5787a4405450646bac3db48cf3077
Robel Fekadu <robel.ugr-4552-16@aau.edu.et>
2025-03-21 15:34:35 +0300

    django

--- django_blog/django_blog/settings.py
+++ django_blog/django_blog/settings.py
@@ -123,7 +123,7 @@ USE_TZ = True
 
 STATIC_URL = 'static/'
 STATICFILES_DIRS = [
-    BASE_DIR / "templates/",
+    BASE_DIR /'blog'/"templates/",
 ]
 
 DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
