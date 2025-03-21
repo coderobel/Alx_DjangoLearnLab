e45797279a9b0f02b7a7327cb10f290ea29aadd7
Robel Fekadu <robel.ugr-4552-16@aau.edu.et>
2025-03-21 19:11:10 +0300

    django

--- django_blog/blog/urls.py
+++ django_blog/blog/urls.py
@@ -24,6 +24,6 @@ urlpatterns = [
     path('', blogpostListView.as_view(), name='blogpost-list'),
     path('<int:pk>/', blogpostDetailView.as_view(), name='blogpost-detail'),
     path('post/new/', blogpostCreateView.as_view(), name='blogpost-create'),
-    path('<int:pk>/update/', blogpostUpdateView.as_view(), name='blogpost-update'),
-    path('<int:pk>/delete/', blogpostDeleteView.as_view(), name='blogpost-delete'),
+    path('post/<int:pk>/update/', blogpostUpdateView.as_view(), name='blogpost-update'),
+    path('post/<int:pk>/delete/', blogpostDeleteView.as_view(), name='blogpost-delete'),
 ]
