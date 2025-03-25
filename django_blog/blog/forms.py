9d08dfa613910b5966e82fafb49807dba3e7bd45
Robel Fekadu <robel.ugr-4552-16@aau.edu.et>
2025-03-25 16:26:46 +0300

    django

--- django_blog/blog/forms.py
+++ django_blog/blog/forms.py
@@ -5,6 +5,7 @@ from django.contrib.auth.models import User
 from taggit.forms import TagField
 from taggit.models import Tag
 from django_select2.forms import Select2TagWidget
+from django.forms.widgets import TextInput
 class CustomUserCreationForm(UserCreationForm):
     email = forms.EmailField(required=True)
 
@@ -45,3 +46,19 @@ class PostForm(forms.ModelForm):
     class Meta:
         model = Post
         fields = ['title', 'content', 'tags']
+        widgets = {
+            'content': forms.Textarea(attrs={'rows': 5}),
+        }
+class TagWidget(TextInput):
+    def __init__(self, attrs=None):
+        default_attrs = {'placeholder': 'Enter tags separated by commas'}
+        if attrs:
+            default_attrs.update(attrs)
+        super().__init__(default_attrs)
+
+    def format_value(self, value):
+        if value is None:
+            return ''
+        if isinstance(value, (list, tuple)):
+            return ', '.join(value)
+        return value
