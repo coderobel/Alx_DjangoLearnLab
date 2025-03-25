105230de564f329319c170b44b869b3ce5a11752
Robel Fekadu <robel.ugr-4552-16@aau.edu.et>
2025-03-25 07:44:35 +0300

    django

--- django_blog/blog/urls.py
+++ django_blog/blog/urls.py
@@ -15,9 +15,9 @@ from .views import (
 )
 
 urlpatterns = [
-   # path('accounts/', include('django.contrib.auth.urls')),
-    #path('accounts/profile', TemplateView.as_view(template_name = 'accounts/profile.html'), name = 'profile'),
-    #path('signup/', SignUpView.as_view(),name = 'templates/registration/signup'),
+    path('accounts/', include('django.contrib.auth.urls')),
+    path('accounts/profile', TemplateView.as_view(template_name = 'accounts/profile.html'), name = 'profile'),
+    path('signup/', SignUpView.as_view(),name = 'templates/registration/signup'),
     path('login/', auth_views.LoginView.as_view(template_name='blog/login.html'), name='login'),
     path('logout/', auth_views.LogoutView.as_view(), name='logout'),
     path('register/', SignUpView.as_view(),  name='register'),
@@ -28,7 +28,8 @@ urlpatterns = [
     path('post/<int:pk>/update/', blogpostUpdateView.as_view(), name='blogpost-update'),
     path('post/<int:pk>/delete/', blogpostDeleteView.as_view(), name='blogpost-delete'),
     path('post/<int:pk>', blogpostDetailView.as_view(), name='blogpost-detail'),
-    path('post/<int:pk>/comment/new', CommentCreateView.as_view, name='add_comment_to_post'),
+    path('post/<int:pk>/comments/new', CommentCreateView.as_view, name='add_comment_to_post'),
     path('comment/<int:pk>/edit/', CommentUpdateView.as_view(), name='comment-update'),
     path('comment/<int:pk>/delete/', CommentDeleteView.as_view(), name='comment-delete'),
+    path('comment/<int:pk>/update/', CommentUpdateView.as_view(), name='comment-update'),
 ]
