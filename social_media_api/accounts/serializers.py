429f1b74ef12b644851d2ef2a00ffa4d80642eb9
Robel Fekadu <robel.ugr-4552-16@aau.edu.et>
2025-03-29 16:59:27 +0300

    django

--- social_media_api/accounts/serializers.py
+++ social_media_api/accounts/serializers.py
@@ -1,6 +1,6 @@
 from rest_framework import serializers
 from .models import Post
-
+from rest_framework.authtoken.models import Token
 class PostSerializer(serializers.ModelSerializer):
     class Meta:
         model = Post
