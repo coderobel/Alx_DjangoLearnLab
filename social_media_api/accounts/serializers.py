7f5849174e3b76163b579072d701280f51c0a990
Robel Fekadu <robel.ugr-4552-16@aau.edu.et>
2025-03-29 17:10:37 +0300

    django

--- social_media_api/accounts/serializers.py
+++ social_media_api/accounts/serializers.py
@@ -8,15 +8,15 @@ class PostSerializer(serializers.ModelSerializer):
     class Meta:
         model = Post
         fields = ['bio', 'profile_picture', 'followers']
-    class UserSerializer(serializers.ModelSerializer):
-        token = serializers.CharField(max_length=255,source='auth_token.key', read_only=True)
+class UserSerializer(serializers.ModelSerializer):
+    token = serializers.CharField(max_length=255,source='auth_token.key', read_only=True)
 
-        class Meta:
-            model = get_user_model()
-            fields = ['username', 'email', 'password', 'token']
-            extra_kwargs = {'password': {'write_only': True}}
+    class Meta:
+        model = get_user_model()
+        fields = ['username', 'email', 'password', 'token']
+        extra_kwargs = {'password': {'write_only': True}}
 
-        def create(self, validated_data):
-            user = get_user_model().objects.create_user(**validated_data)
-            Token.objects.create(user=user)
-            return user
+    def create(self, validated_data):
+        user = get_user_model().objects.create_user(**validated_data)
+        Token.objects.create(user=user)
+        return user
